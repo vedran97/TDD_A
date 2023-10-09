dc900ae8f4615e773c37bf99b499c89c754e5090
VEDANT RANADE <vedantmranade@gmail.com>
2023-10-09 03:00:42 -0400

    Fixed Tests
    Fixed a logical error in design of tests

--- test/test.cpp
+++ test/test.cpp
@@ -7,12 +7,12 @@
 TEST(pid_test_1, proportional_test) {
   controller::Gains gains(1.0, 0.0, 0.0, 0.0);
   controller::PIDController pid(gains);
-  EXPECT_FLOAT_EQ(1.0, pid.compute(1, 0));
+  EXPECT_FLOAT_EQ(1.0, pid.compute(-1, 0));
 }
 
 // @brief: Test to check if the controller output is 1 for kp=0 kd=0 ki=1
 TEST(pid_test_2, integral_test) {
   controller::Gains gains(0.0, 0.0, 1.0, 10);
   controller::PIDController pid(gains);
-  EXPECT_FLOAT_EQ(1.0, pid.compute(1, 0));
+  EXPECT_FLOAT_EQ(1.0, pid.compute(-1, 0));
 }
