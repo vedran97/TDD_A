221e118abefc399435286ff365c596ae00860373
VEDANT RANADE <vedantmranade@gmail.com>
2023-10-09 02:32:56 -0400

    Added test 2, modified PID Class impl
    Added another test
    Initialized PIDController variables

--- app/PID.cpp
+++ app/PID.cpp
@@ -5,7 +5,11 @@ namespace controller {
  * @brief Store controller gains
  * @param[in] inputGains Gains struct
  */
-PIDController::PIDController(controller::Gains inputGains) { ; }
+PIDController::PIDController(controller::Gains inputGains) {
+  gains = inputGains;
+  motor_integral = 0;
+  motor_prev_error = 0;
+}
 
 float PIDController::compute(float inputVel, float targetVel) { return 0.0; }
 
--- test/test.cpp
+++ test/test.cpp
@@ -5,7 +5,14 @@
 // @brief: Test to check if the controller output is 1.0 for kp=1.0 kd=0.0
 // ki=0.0
 TEST(pid_test_1, proportional_test) {
-  controller::Gains gains;
+  controller::Gains gains(1.0, 0.0, 0.0, 0.0);
+  controller::PIDController pid(gains);
+  EXPECT_FLOAT_EQ(1.0, pid.compute(1, 0));
+}
+
+// @brief: Test to check if the controller output is 1 for kp=0 kd=0 ki=1
+TEST(pid_test_2, integral_test) {
+  controller::Gains gains(0.0, 0.0, 1.0, 10);
   controller::PIDController pid(gains);
   EXPECT_FLOAT_EQ(1.0, pid.compute(1, 0));
 }
